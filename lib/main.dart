d5809122a417a4b3c4b2117ed457fe8c8dc0aef5
PwS <pawanasutha02@gmail.com>
2022-09-18 23:51:25 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:counter_app/cubits/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'other_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'My Counter',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: const MyHomePageUsingExtensionContext(),
+      ),
+    );
+  }
+}
+
+class MyHomePageUsingBloc extends StatelessWidget {
+  const MyHomePageUsingBloc({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocConsumer<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.counter == 3) {
+            showDialog(
+                context: context,
+                builder: (counter) => AlertDialog(
+                      content: Text('Counter is ${state.counter}'),
+                    ));
+          } else if (state.counter == -1) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => OtherPage()));
+          }
+        },
+        builder: (context, state) {
+          return Center(
+            child: Text('${state.counter}'),
+          );
+        },
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              BlocProvider.of<CounterCubit>(context).increment();
+            },
+            child: Icon(Icons.add),
+            heroTag: 'Increment',
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              BlocProvider.of<CounterCubit>(context).decrement();
+            },
+            child: Icon(Icons.remove),
+            heroTag: 'Decrement',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyHomePageUsingExtensionContext extends StatelessWidget {
+  const MyHomePageUsingExtensionContext({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocConsumer<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.counter == 3) {
+            showDialog(
+                context: context,
+                builder: (counter) => AlertDialog(
+                      content: Text('Counter is ${state.counter}'),
+                    ));
+          } else if (state.counter == -1) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => OtherPage()));
+          }
+        },
+        builder: (context, state) {
+          return Center(
+            child: Text('${state.counter}'),
+          );
+        },
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              context.read<CounterCubit>().increment();
+            },
+            child: Icon(Icons.add),
+            heroTag: 'Increment',
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              context.read<CounterCubit>().decrement();
+            },
+            child: Icon(Icons.remove),
+            heroTag: 'Decrement',
+          ),
+        ],
+      ),
+    );
+  }
+}
